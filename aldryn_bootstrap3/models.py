bcb25ca8e7ac5bc7d72845af225fb2b6c4a7f509
Amos Vryhof <amos@vryhofresearch.com>
2021-12-29 12:00:55 -0500

    remove python_2_unicode_compatible

--- aldryn_bootstrap3/models.py
+++ aldryn_bootstrap3/models.py
@@ -10,7 +10,6 @@ import djangocms_text_ckeditor.fields
 import filer.fields.folder
 from cms.models.pluginmodel import CMSPlugin
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.html import strip_tags
 from django.utils.translation import ugettext, ugettext_lazy as _, ungettext
 from djangocms_attributes_field.fields import AttributesField
@@ -34,7 +33,6 @@ from . import model_fields, constants
 # [✓] Responsive utilities
 
 
-@python_2_unicode_compatible
 class Bootstrap3RowPlugin(CMSPlugin):
     """
     CSS - Grid system: "Row" Model
@@ -63,7 +61,6 @@ class Bootstrap3RowPlugin(CMSPlugin):
         return column_count_str
 
 
-@python_2_unicode_compatible
 class Bootstrap3ColumnPlugin(CMSPlugin):
     """
     CSS - Grid system: "Column" Model
@@ -130,7 +127,6 @@ for size, name in constants.DEVICE_CHOICES:
     )
 
 
-@python_2_unicode_compatible
 class Boostrap3BlockquotePlugin(CMSPlugin):
     """
     CSS - Typography: "Blockquote" Model
@@ -154,7 +150,6 @@ class Boostrap3BlockquotePlugin(CMSPlugin):
         return ""
 
 
-@python_2_unicode_compatible
 class Boostrap3CitePlugin(CMSPlugin):
     """
     CSS - Typography: "Cite" Model
@@ -170,7 +165,6 @@ class Boostrap3CitePlugin(CMSPlugin):
         return ""
 
 
-@python_2_unicode_compatible
 class Bootstrap3CodePlugin(CMSPlugin):
     """
     CSS - Code: Model
@@ -198,7 +192,6 @@ class Bootstrap3CodePlugin(CMSPlugin):
         return "<{}>".format(self.code_type)
 
 
-@python_2_unicode_compatible
 class Boostrap3ButtonPlugin(CMSPlugin, model_fields.LinkMixin):
     """
     CSS - Buttons: "Button/Link" Model
@@ -231,7 +224,6 @@ class Boostrap3ButtonPlugin(CMSPlugin, model_fields.LinkMixin):
         return self.label
 
 
-@python_2_unicode_compatible
 class Boostrap3ImagePlugin(CMSPlugin):
     """
     CSS - Images: Model
@@ -339,7 +331,6 @@ class Boostrap3ImagePlugin(CMSPlugin):
         return items
 
 
-@python_2_unicode_compatible
 class Bootstrap3ResponsivePlugin(CMSPlugin):
     """
     CSS - Responsive: "Utilities" Model
@@ -391,7 +382,6 @@ class Bootstrap3ResponsivePlugin(CMSPlugin):
 # [✓] Wells
 
 
-@python_2_unicode_compatible
 class Boostrap3IconPlugin(CMSPlugin):
     """
     Component - Glyphicons: "Icon" Model
@@ -410,7 +400,6 @@ class Boostrap3IconPlugin(CMSPlugin):
         return self.icon
 
 
-@python_2_unicode_compatible
 class Boostrap3LabelPlugin(CMSPlugin):
     """
     Component - Label: Model
@@ -433,7 +422,6 @@ class Boostrap3LabelPlugin(CMSPlugin):
         return self.label
 
 
-@python_2_unicode_compatible
 class Boostrap3JumbotronPlugin(CMSPlugin):
     """
     Component - Jumbotron: Model
@@ -456,7 +444,6 @@ class Boostrap3JumbotronPlugin(CMSPlugin):
         return self.label or str(self.pk)
 
 
-@python_2_unicode_compatible
 class Boostrap3AlertPlugin(CMSPlugin):
     """
     Component - Alert: Model
@@ -502,7 +489,6 @@ class Bootstrap3ListGroupPlugin(CMSPlugin):
         return column_count_str
 
 
-@python_2_unicode_compatible
 class Bootstrap3ListGroupItemPlugin(CMSPlugin):
     """
     Component - List group: "Item" Model
@@ -531,7 +517,6 @@ class Bootstrap3ListGroupItemPlugin(CMSPlugin):
         return self.title
 
 
-@python_2_unicode_compatible
 class Boostrap3PanelPlugin(CMSPlugin):
     """
     Component - Panel: "Wrapper" Model
@@ -553,7 +538,6 @@ class Boostrap3PanelPlugin(CMSPlugin):
         return self.context
 
 
-@python_2_unicode_compatible
 class Boostrap3PanelHeadingPlugin(CMSPlugin):
     """
     Component - Panel: "Heading" Model
@@ -572,7 +556,6 @@ class Boostrap3PanelHeadingPlugin(CMSPlugin):
         return self.title
 
 
-@python_2_unicode_compatible
 class Boostrap3PanelBodyPlugin(CMSPlugin):
     """
     Component - Panel: "Body" Model
@@ -588,7 +571,6 @@ class Boostrap3PanelBodyPlugin(CMSPlugin):
         return self.classes
 
 
-@python_2_unicode_compatible
 class Boostrap3PanelFooterPlugin(CMSPlugin):
     """
     Component - Panel: "Footer" Model
@@ -604,7 +586,6 @@ class Boostrap3PanelFooterPlugin(CMSPlugin):
         return self.classes
 
 
-@python_2_unicode_compatible
 class Boostrap3WellPlugin(CMSPlugin):
     """
     Component - Wells: Model
@@ -641,7 +622,6 @@ class Boostrap3WellPlugin(CMSPlugin):
 # [ ] Affix
 
 
-@python_2_unicode_compatible
 class Bootstrap3TabPlugin(CMSPlugin):
     """
     JavaScript - Tab: "Wrapper" Model
@@ -677,7 +657,6 @@ class Bootstrap3TabPlugin(CMSPlugin):
         return "{} {}".format(self.style, self.effect)
 
 
-@python_2_unicode_compatible
 class Bootstrap3TabItemPlugin(CMSPlugin):
     """
     JavaScript - Tab: "Item" Model
@@ -695,7 +674,6 @@ class Bootstrap3TabItemPlugin(CMSPlugin):
         return self.title
 
 
-@python_2_unicode_compatible
 class Bootstrap3AccordionPlugin(CMSPlugin):
     """
     JavaScript - Collapse: "Accordion" Model
@@ -727,7 +705,6 @@ class Bootstrap3AccordionPlugin(CMSPlugin):
         return str(self.index)
 
 
-@python_2_unicode_compatible
 class Bootstrap3AccordionItemPlugin(CMSPlugin):
     """
     JavaScript - Collapse: "Accordion item" Model
@@ -750,7 +727,6 @@ class Bootstrap3AccordionItemPlugin(CMSPlugin):
         return self.title
 
 
-@python_2_unicode_compatible
 class Bootstrap3CarouselPlugin(CMSPlugin):
     """
     JavaScript - Carousel: "Wrapper" Model
@@ -863,7 +839,6 @@ class Bootstrap3CarouselPlugin(CMSPlugin):
         return items
 
 
-@python_2_unicode_compatible
 class Bootstrap3CarouselSlidePlugin(CMSPlugin, model_fields.LinkMixin):
     """
     JavaScript - Carousel: "Slide" Model
@@ -907,7 +882,6 @@ class Bootstrap3CarouselSlidePlugin(CMSPlugin, model_fields.LinkMixin):
             return image_text or content_text
 
 
-@python_2_unicode_compatible
 class Bootstrap3CarouselSlideFolderPlugin(CMSPlugin):
     """
     JavaScript - Carousel: "Slide folder" Model
@@ -932,7 +906,6 @@ class Bootstrap3CarouselSlideFolderPlugin(CMSPlugin):
 # [✓] File
 
 
-@python_2_unicode_compatible
 class Boostrap3SpacerPlugin(CMSPlugin):
     """
     Custom - Spacer: Model
@@ -948,7 +921,6 @@ class Boostrap3SpacerPlugin(CMSPlugin):
         return "size-" + self.size + " " + self.classes
 
 
-@python_2_unicode_compatible
 class Bootstrap3FilePlugin(CMSPlugin):
     """
     Custom - File: Model
