bfbc12195fa582d6b198f5007c7671fb6ed7697e
Amos Vryhof <amos@vryhofresearch.com>
2022-06-21 09:28:22 -0400

    Add --upload option to build script

--- build_release.sh
+++ build_release.sh
@@ -2,7 +2,9 @@
 pip install --upgrade setuptools
 pip install --upgrade wheel
 pip install --upgrade twine
-python setup.py sdist bdist_wheel
+if ["$1" != "--upload"]; then
+  python setup.py sdist bdist_wheel
+fi
 # Test PyPi
 #twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 # Prod PyPi
